ebd5479264fce859f2f1963e1c5b802f37a29d9d
HOCgaming <HCash1@sheffield.ac.uk>
2019-05-04 22:21:39 +0100

    Camera

---
+++ Heist Noire/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Vector3 offset;
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce((player.transform.position + offset) - transform.position);
+    }
+}
