3f0beb1f470b9467b386c91ba78e55c12edf1007
Owen Lyons <owenlyons98@gmail.com>
2019-05-04 12:13:03 +0100

    added basic movement system

--- Heist Noire/Assets/Player.cs
+++ Heist Noire/Assets/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+
+    private Rigidbody rb;
+    public float speed = 10;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        InputScript();
+    }
+
+    void InputScript()
+    {
+        Vector3 movement_vector = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
+
+        rb.MovePosition(rb.position + (movement_vector * speed) * Time.deltaTime);
         
     }
 }
